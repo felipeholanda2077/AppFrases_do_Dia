f6c924ea1994caef31037f260cb9c45cff84e052
felipeholanda2077 <felipe_holandafreitas@hotmail.com>
2022-08-22 15:06:26 -0300

    Finalizado

---
+++ android/app/src/main/kotlin/com/example/mensagem_diaria/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mensagem_diaria
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  var _frases = [
+    "Frase1",
+    "Frase2",
+    "Frase3",
+    "Frase4",
+  ];
+
+  var _fraseGerada = "Clique abaixo para gerar uma frase!";
+
+  void _gerarFrase(){
+
+    var numeroSorteado = Random().nextInt(_frases.length);
+
+    setState(() {
+      _fraseGerada = _frases[ numeroSorteado ];
+    });
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Frases do Dia"),
+        backgroundColor: Colors.green,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        width: double.infinity,
+        /*decoration: BoxDecoration(
+          border: Border.all(width: 3, color: Colors.amber)
+        ),*/
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset("images/logo.png"),
+            Text(
+              _fraseGerada,
+              textAlign: TextAlign.justify,
+              style: TextStyle(
+                fontSize: 17,
+                fontStyle: FontStyle.italic,
+                color: Colors.black
+              ),
+            ),
+            RaisedButton(
+              child: Text(
+                "Nova Frase",
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              color: Colors.green,
+              onPressed: _gerarFrase,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
